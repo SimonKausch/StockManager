d41415b9cb505f0ee1924a405cb7cbc352ffe311
Simon Kausch <simon.kausch@protonmail.ch>
2025-03-29 13:45:25 +0100

    Add Stock struct and database operations for inserting and listing stock items

--- database.go
+++ database.go
@@ -7,6 +7,16 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
+type Stock struct {
+	ID              int64
+	XLength         int
+	YLength         int
+	ZLength         int
+	Material        string
+	CertificatePath string
+	InvoicePath     string
+}
+
 func initDB() (*sql.DB, error) {
 	db, err := sql.Open("sqlite3", "stock.db")
 	if err != nil {
@@ -30,4 +40,32 @@ func initDB() (*sql.DB, error) {
 	}
 
 	return db, nil
+}
+
+func insertStock(db *sql.DB, stock *Stock) error {
+	query := `
+		INSERT INTO stock (xLength, yLength, zLength, material, certificate_path, invoice_path)
+		VALUES (?, ?, ?, ?, ?, ?)
+	`
+	_, err := db.Exec(query, stock.XLength, stock.YLength, stock.ZLength, stock.Material, stock.CertificatePath, stock.InvoicePath)
+	return err
+}
+
+func ListStock(db *sql.DB) ([]Stock, error) {
+	row, err := db.Query("SELECT * FROM stock ORDER BY id")
+	if err != nil {
+		return nil, err
+	}
+	defer row.Close()
+
+	stockMaterials := []Stock{}
+	for row.Next() {
+		var stock Stock
+		err := row.Scan(&stock.ID, &stock.XLength, &stock.YLength, &stock.ZLength, &stock.Material, &stock.CertificatePath, &stock.InvoicePath)
+		if err != nil {
+			return nil, err
+		}
+		stockMaterials = append(stockMaterials, stock)
+	}
+	return stockMaterials, nil
 }
